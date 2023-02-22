f1b6af651056eecea814fc4888e866a17d1fb124
Badr <badrelamine1998@gmail.com>
2023-02-22 11:03:39 -0800

    7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Check main
+ * @r: Input
+ * Description: return value of number
+ * Return: r
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
