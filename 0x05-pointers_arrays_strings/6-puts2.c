2ba3670c64e179548868124e17a70a1e8ed6751b
Badr <badrelamine1998@gmail.com>
2023-02-28 05:30:01 -0800

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - function that print every
+ * othet char of the string
+ * @str: input
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	len -= 1;
+
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
